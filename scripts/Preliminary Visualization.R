b9acbda4e58a3e513f7cb62be25831b4fd6658ec
coltonwagner <63270532+coltonwagner@users.noreply.github.com>
2021-02-12 01:14:52 -0500

    Update Preliminary Visualization.R

--- scripts/Preliminary Visualization.R
+++ scripts/Preliminary Visualization.R
@@ -76,3 +76,65 @@ ggplot(grouped_gdp, aes(x = TIME, y = Value, color = LOCATION)) +
   scale_x_yearqtr(format = "%Y-Q%q") +
   labs(x = "Quarter", y = "Change in Quarterly GDP (Percent)", title = "GDP Percent Change from Preceding Quarter Across Major Economic Groupings") +
   t9theme
+
+
+## STOCK DATA
+
+## load from csv files 
+can <- read_csv("Stock_Canada_GSPTSE.csv")
+fra <- read_csv("Stock_France_FCHI.csv")
+ger <- read_csv("Stock_Germany_GDAXI.csv")
+ita <- read_csv("Stock_Italy_FTSEMIB.MI.csv")
+jap <- read_csv("Stock_Japan_N225.csv")
+uk <- read_csv("Stock_UK_FTAS.csv")
+us <- read_csv("Stock_US_GSPC.csv")
+
+## add column to store country/index information
+can$country <- 'CAN - GSPTSE'
+fra$country <- 'FRA - FCHI'
+ger$country <- 'GER - GDAXI'
+ita$country <- 'ITA - FTSEMIB'
+jap$country <- 'JAP - N225'
+uk$country <- 'UK - FTAS'
+us$country <- 'US - GSPC'
+
+## fixing some slight date and data type errors
+fra$Date <- as.Date(fra$Date, "%m/%d/%Y")
+ger$Date <- as.Date(ger$Date, "%m/%d/%Y")
+jap$Date <- as.Date(jap$Date, "%m/%d/%Y")
+fra$Close <- as.numeric(fra$Close)
+ita$Close <- as.numeric(ita$Close)
+uk$Close <- as.numeric(uk$Close)
+
+## merge all stock info into a single dataset
+stocks <- rbind(can, fra, ger, ita, jap, uk, us)
+stocks <- na.omit(stocks)
+
+# create a column to index stock prices
+# need to start at 12-04-2020 due to lack of uk and italy data 
+can$close_index <- can$Close / (can$Close[can$Date==as.Date("2019-12-04")])
+fra$close_index <- fra$Close / (fra$Close[fra$Date==as.Date("2019-12-04")])
+ger$close_index <- ger$Close / (ger$Close[ger$Date==as.Date("2019-12-04")])
+ita$close_index <- ita$Close / (ita$Close[ita$Date==as.Date("2019-12-04")])
+jap$close_index <- jap$Close / (jap$Close[jap$Date==as.Date("2019-12-04")])
+uk$close_index <- uk$Close / (uk$Close[uk$Date==as.Date("2019-12-04")])
+us$close_index <- us$Close / (us$Close[us$Date==as.Date("2019-12-04")])
+
+# merge all indexed stock info together
+stocks_indexed <- rbind(can, fra, ger, ita, jap, uk, us)
+stocks_indexed <- na.omit(stocks_indexed)
+stocks_indexed <- subset(stocks_indexed, Date >= as.Date("2019-12-04"))
+
+## overall plot for each country's stock index
+ggplot(stocks, aes(x = Date, y = Close, , color = country)) +
+  geom_line() +
+  labs(x = "Date", y = "Close Price", title = "Stock Indexes Across Major Countries", subtitle = "No Modifications to Data, Different Currencies") +
+  scale_x_date(date_breaks = "4 months", date_labels = "%b %Y") +
+  t9theme
+
+## overall plot for each country's stock index (indexed to 12-04-2019)
+ggplot(stocks_indexed, aes(x = Date, y = close_index, , color = country)) +
+  geom_line() +
+  labs(x = "Date", y = "Close Price (Indexed)", title = "Stock Indexes Across Major Countries", subtitle = "Indexed Close Prices to 12-04-2019") +
+  scale_x_date(date_breaks = "2 months", date_labels = "%b %Y") +
+  t9theme
